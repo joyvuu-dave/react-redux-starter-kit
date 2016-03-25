230a96b738bf1e133a5c427be8cdb52d91703767
Dave Riddle <david@joyvuu.com>
2016-03-25 08:43:23 -0700

    add flow declaration to reducer blueprint

--- blueprints/reducer/index.js
+++ blueprints/reducer/index.js
@@ -1,6 +1,12 @@
 const path = require('path')
 
 module.exports = {
+  locals: function(options) {
+    return {
+      flowDeclaration: '@flow'
+    };
+  },
+
   // Should probably never need to be overriden
   filesPath: function () {
     return path.join(this.path, 'files')
